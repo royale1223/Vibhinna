07a1211349a907045b5e7edc65f58cad6eff37da
Binoy Babu <royale1223@gmail.com>
2002-01-01 07:05:00 +0530

    helper : add scan virtual systems

--- src/com/vibhinna/binoy/DataBaseHelper.java
+++ src/com/vibhinna/binoy/DataBaseHelper.java
@@ -54,5 +54,99 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
 	}
 
+	@Override
+	public void onOpen(SQLiteDatabase db) {
+		// tbd
+		if (db.isReadOnly()) {
+			Log.d(TAG, "db is readonly");
+			return;
+		}
+		Log.d(TAG, "db is writable");
+		String extState = Environment.getExternalStorageState();
+		if (extState.equals(Environment.MEDIA_MOUNTED)) {
+			FileFilter filterDirectoriesOnly = new FileFilter() {
+				@Override
+				public boolean accept(File file) {
+					return file.isDirectory();
+				}
+			};
+			Cursor pathcursora = db.query(DataBaseHelper.VFS_DATABASE_TABLE,
+					new String[] { DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_PATH,
+							BaseColumns._ID }, null, null, null, null, null);
+			if (!Constants.MBM_ROOT.exists()) {
+				Constants.MBM_ROOT.mkdir();
+			}
+			if (pathcursora.getCount() > 0) {
+				// scan whole db and get path to cursor
+				pathcursora.moveToFirst();
+				do {
+					File cfile = new File(pathcursora.getString(0));
+					if (!cfile.exists()) {
+						// remove invalid db files
+						db.delete(DataBaseHelper.VFS_DATABASE_TABLE,
+								BaseColumns._ID + " IS ?",
+								new String[] { pathcursora.getString(1) });
+						// writeXML();
+					}
+				} while (pathcursora.moveToNext());
+			}
+			pathcursora.close();
+			File[] sdDirectories = Constants.MBM_ROOT
+					.listFiles(filterDirectoriesOnly);
+			// get all dirs in /mnt/sdcard/multiboot
+			for (int i = 0; i < sdDirectories.length; i++) {
+				File file = sdDirectories[i];
+				String vspathi = null;
+				try {
+					vspathi = file.getCanonicalPath();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				String vsname = file.getName();
+				Cursor pathcursorb = db
+				// compare with db entries
+						.rawQuery("SELECT " + BaseColumns._ID + " FROM "
+								+ DataBaseHelper.VFS_DATABASE_TABLE + " WHERE "
+								+ DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_PATH
+								+ "=?", new String[] { vspathi });
+				// filter out those with a . as prefix
+				if (!vsname.startsWith(".")) {
+					// if cursor is empty, the vs is not registerd
+					if (pathcursorb.getCount() == 0) {
+						ContentValues values = new ContentValues();
+						values.put(DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_NAME,
+								vsname);
+						try {
+							values.put(
+									DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_PATH,
+									file.getCanonicalPath());
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						try {
+							values.put(
+									DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_DESCRIPTION,
+									context.getString(R.string.newvfsi)
+											+ file.getCanonicalPath() + ")");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						values.put(DataBaseHelper.VIRTUAL_SYSTEM_COLUMN_TYPE,
+								"2");
+						db.insert(DataBaseHelper.VFS_DATABASE_TABLE, null,
+								values);
+					}
+				}
+				pathcursorb.close();
+			}
+			return;
+		} else if (extState.equals(Environment.MEDIA_MOUNTED_READ_ONLY)) {
+			return;
+		} else {
+			return;
+		}
+	}
 }
-
