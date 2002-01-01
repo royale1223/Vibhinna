ed22baafd271913da66627b3fe8f943a986208b9
Binoy Babu <royale1223@gmail.com>
2002-01-01 06:52:35 +0530

    add ProcessManager.java

---
+++ src/com/vibhinna/binoy/ProcessManager.java
@@ -0,0 +1,69 @@
+package com.vibhinna.binoy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+import android.util.Log;
+
+public class ProcessManager {
+	private static final String TAG = null;
+	private InputStream inputstream;
+
+	public String errorStreamReader(String[] shellinput) {
+		// TODO use pb instead of runtime
+		InputStream inputstream = null;
+		String esrval = "";
+		// hack to prevent executing null
+		for (int i = 0; i <= 3; i++) {
+			if (shellinput[i] == null) {
+				shellinput[i] = "";
+			}
+		}
+		try {
+			inputstream = Runtime
+					.getRuntime()
+					.exec(shellinput[0] + shellinput[1] + shellinput[2]
+							+ shellinput[3]).getErrorStream();
+			InputStreamReader inputstreamreader = new InputStreamReader(
+					inputstream);
+			esrval = new BufferedReader(inputstreamreader).readLine();
+		} catch (IOException e) {
+			Log.w(TAG, "Error in errorStreamReader()");
+		}
+		Log.i("execout", esrval + "");
+		return esrval + "";
+
+	}
+
+
+	public String inputStreamReader(String[] shellinput, int length) {
+		// TODO use pb instead of runtime
+		for (int i = 0; i < shellinput.length; i++) {
+			if (shellinput[i] == null) {
+				shellinput[i] = "";
+			}
+		}
+		try {
+			inputstream = Runtime
+					.getRuntime()
+					.exec(shellinput[0] + shellinput[1] + shellinput[2]
+							+ shellinput[3]).getInputStream();
+		} catch (IOException e) {
+			Log.w("IOException", "exception in executing");
+		}
+		String isrstr = convertStreamToString(inputstream);
+		return isrstr;
+	}
+
+	public String convertStreamToString(InputStream is) {
+		try {
+			return new Scanner(is).useDelimiter("\\A").next();
+		} catch (NoSuchElementException e) {
+			return "";
+		}
+	}
+}
