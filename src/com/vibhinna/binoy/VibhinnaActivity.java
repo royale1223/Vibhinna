5f9baf6765e4957844edcdc5ec1eb9daab00e130
Binoy Babu <royale1223@gmail.com>
2012-05-08 03:49:00 +0530

    rename fragment.xml

--- src/com/vibhinna/binoy/VibhinnaActivity.java
+++ src/com/vibhinna/binoy/VibhinnaActivity.java
@@ -8,6 +8,6 @@ public class VibhinnaActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment);
+		setContentView(R.layout.main_fragment);
 	}
 }
