a7346a21ae019fbc8277b42127c7de055ddbb518
Binoy Babu <royale1223@gmail.com>
2012-06-23 10:54:03 +0530

    organize imports

--- src/com/vibhinna/binoy/VibhinnaFragment.java
+++ src/com/vibhinna/binoy/VibhinnaFragment.java
@@ -5,7 +5,6 @@ import java.io.File;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
